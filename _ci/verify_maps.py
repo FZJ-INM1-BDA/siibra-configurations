b24e3e0ff4dc80f008d84f990a858067f2d254de
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2023-05-23 13:15:30 +0200

    add ci to check map region names in parcellations

--- _ci/verify_maps.py
+++ _ci/verify_maps.py
@@ -6,6 +6,48 @@ path_to_maps = Path("maps")
 path_to_space = Path("spaces")
 path_to_parcellations = Path("parcellations")
 
+REMOVE_FROM_NAME = [
+    "hemisphere",
+    " -",
+    "-brain",
+    "both",
+    "Both",
+]
+
+REPLACE_IN_NAME = {
+    "ctx-lh-": "left ",
+    "ctx-rh-": "right ",
+}
+
+
+def clear_name(name):
+    """ clean up a region name to the for matching"""
+    result = name
+    for word in REMOVE_FROM_NAME:
+        result = result.replace(word, "")
+    for search, repl in REPLACE_IN_NAME.items():
+        result = result.replace(search, repl)
+    return " ".join(w for w in result.split(" ") if len(w))
+
+def get_parcellation_region_names(region_tree):
+    region_names = []
+    for region in region_tree:
+        for key in region.keys():
+            if key == "name":
+                region_names.append(clear_name(region["name"]))
+            if key == "children":
+                children_names = get_parcellation_region_names(region["children"])
+                region_names.extend(children_names)
+    return region_names
+
+def compare_regions_to_parcellation(map_json, parc_region_tree):
+    unmatched_regions = []
+    parc_regions = get_parcellation_region_names(parc_region_tree)
+    for region in map_json.get("indices").keys():
+        if clear_name(region) not in parc_regions:
+            unmatched_regions.append(region)
+    return unmatched_regions
+
 def get_json(path: Path):
     for dirname, _, filenames in os.walk(path):
         for filename in filenames:
@@ -16,10 +58,10 @@ def get_json(path: Path):
                 yield json.load(fp=fp), str(file)
 
 def main():
-    parc_ids = set()
+    parcs = dict()
     space_ids = set()
     for json_obj, _ in get_json(path_to_parcellations):
-        parc_ids.add(json_obj.get("@id"))
+        parcs[json_obj.get("@id")] = json_obj.get("regions")
     for json_obj, _ in get_json(path_to_space):
         space_ids.add(json_obj.get("@id"))
     for json_obj, filepath in get_json(path_to_maps):
@@ -28,8 +70,10 @@ def main():
         parc_id = json_obj.get("parcellation", {}).get("@id")
         assert space_id, f"{errmsg}, space id not defined!"
         assert parc_id, f"{errmsg}, parc id not defined!"
-        assert parc_id in parc_ids, f"{errmsg}, parc id for {parc_id} not found in parcellations"
+        assert parc_id in parcs.keys(), f"{errmsg}, parc id for {parc_id} not found in parcellations"
         assert space_id in space_ids, f"{errmsg}, space id for {space_id} not found in spaces"
+        unmatched_regions = compare_regions_to_parcellation(json_obj, parcs[parc_id])
+        assert len(unmatched_regions) == 0, f"Following regions in {filepath} have no correspondence in parcellation {parc_id}: {unmatched_regions}"
 
 if __name__ == "__main__":
     main()
