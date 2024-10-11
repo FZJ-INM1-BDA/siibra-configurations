67ab55e809dee3fc1fdb945a6c4525fc7a15d10e
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-10-11 13:40:51 +0200

    fix jba3 connectivity config creation script

--- _code/add_jba3_HCP_connectivity_and_bold.py
+++ _code/add_jba3_HCP_connectivity_and_bold.py
@@ -1,8 +1,6 @@
 import siibra
 import json
 import os
-from siibra.retrieval.requests import EbrainsRequest, DECODERS
-from typing import Tuple, List, Dict
 import re
 from io import BytesIO
 
@@ -14,42 +12,51 @@ conn_zip_url = "https://data-proxy.ebrains.eu/api/v1/buckets/reference-atlas-dat
 bold_zip_url = "https://data-proxy.ebrains.eu/api/v1/buckets/reference-atlas-data/temp/julich_brain_3/314-Julich-Brain_BOLD.zip"
 
 req = siibra.retrieval.requests.ZipfileRequest(
-    conn_zip_url,
-    "314-JuBrain/0ImageProcessing/Link.txt",
-    func=lambda b: b
+    conn_zip_url, "314-JuBrain/0ImageProcessing/Link.txt", func=lambda b: b
 )
 REGION_LIST = [
     line.decode().strip().split(maxsplit=1)[-1]
     for line in BytesIO(req.data).readlines()
-    if re.match(r'^\d+ +\w+', line.decode())
+    if re.match(r"^\d+ +\w+", line.decode())
 ]
 
+
 def create_jba3_duplicate(
-        config: dict, zip_url: str, str2replace: str, dataset_uuid: str
+    config: dict, zip_url: str, str2replace: str, dataset_uuid: str
+):
+    if (
+        config.get("parcellation").get("@id")
+        != "minds/core/parcellationatlas/v1.0.0/94c1125b-b87e-45e4-901c-00daee7f2579-290"
     ):
-    if config.get('parcellation').get("@id") != "minds/core/parcellationatlas/v1.0.0/94c1125b-b87e-45e4-901c-00daee7f2579-290":
         return None
     else:
-        config.get('parcellation')["@id"] = "minds/core/parcellationatlas/v1.0.0/94c1125b-b87e-45e4-901c-00daee7f2579-300"
-    if config.get('cohort') != 'HCP':
+        config.get("parcellation")[
+            "@id"
+        ] = "minds/core/parcellationatlas/v1.0.0/94c1125b-b87e-45e4-901c-00daee7f2579-300"
+    if config.get("cohort") != "HCP":
         return None
-    config_dataset_uuid = config.get('ebrains').get("openminds/Dataset")
+    config_dataset_uuid = config.get("ebrains").get("openminds/Dataset")
     if config_dataset_uuid is not None:
         if config_dataset_uuid != dataset_uuid:
             return None
 
-    _ = config.get('ebrains').pop("openminds/DatasetVersion")  # replace this with dataset version uuid when it is released
+    _ = config.get("ebrains").pop(
+        "openminds/DatasetVersion"
+    )  # replace this with dataset version uuid when it is released
 
     config.get("repository")["url"] = zip_url
-    config['files'] = {sub: file.replace("294-Julich-Brain", str2replace) for sub, file in config.get('files').items()}
-    config['regions'] = REGION_LIST
+    config["files"] = {
+        sub: file.replace("294-Julich-Brain", str2replace)
+        for sub, file in config.get("files").items()
+    }
+    config["regions"] = REGION_LIST
     return config
 
 
 def main():
     conn_dataset_uuid = "0f1ccc4a-9a11-4697-b43f-9c9c8ac543e6"
     connectivity_folder = f"{LOCAL_CONFIG_FOLDER}/features/connectivity/regional"
-    
+
     for conn_type in os.listdir(connectivity_folder):
         conn_type_folder = os.path.join(connectivity_folder, conn_type)
         for file in os.listdir(conn_type_folder):
@@ -63,9 +70,8 @@ def main():
                 continue
             else:
                 with open(file_path.replace("2_9", "3_0"), "w") as fp:
-                    json.dump(config, fp, indent="\t")
+                    json.dump(jba3_config, fp, indent="\t")
 
-    BOLD_dataset_uuid = "0f1ccc4a-9a11-4697-b43f-9c9c8ac543e6"
     BOLD_folder = f"{LOCAL_CONFIG_FOLDER}/features/tabular/activity_timeseries/bold"
     for file in os.listdir(BOLD_folder):
         file_path = os.path.join(BOLD_folder, file)
@@ -78,7 +84,8 @@ def main():
             continue
         else:
             with open(file_path.replace("2_9", "3_0"), "w") as fp:
-                json.dump(config, fp, indent="\t")
+                json.dump(jba3_config, fp, indent="\t")
+
 
 if __name__ == "__main__":
     main()
