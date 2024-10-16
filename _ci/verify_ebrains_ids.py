9a47853518e8b28955b3a0f5387c4d92e2c6d7ac
Ahmet Nihat Simsek <ahmet.n.simsek@gmail.com>
2024-10-16 10:55:15 +0200

    flake8 rules and satisfy flake8 in ci code

--- _ci/verify_ebrains_ids.py
+++ _ci/verify_ebrains_ids.py
@@ -1,19 +1,20 @@
 from dataclasses import dataclass
 from urllib.parse import quote
 import requests
-from requests import HTTPError
 import os
 import json
 from typing import Union, Dict, List
 
 try:
     import tqdm
-    tqdm_imported=True
-except ImportError:
-    tqdm_imported=False
 
-PER_PAGE=50
-FAIL_FAST=True
+    tqdm_imported = True
+except ImportError:
+    tqdm_imported = False
+
+PER_PAGE = 50
+FAIL_FAST = True
+
 
 @dataclass
 class EbrainsIdValidationResult:
@@ -21,34 +22,41 @@ class EbrainsIdValidationResult:
     key: str
 
     value: str
-    filename: str=None
+    filename: str = None
+
 
 @dataclass
 class GitlabConnector:
     host: str
     project: int
-    ref: str="master"
+    ref: str = "master"
 
-    def file(self, path:str):
+    def file(self, path: str):
         quoted_path = quote(path, safe="")
-        resp = requests.get(f"{self.host}/api/v4/projects/{self.project}/respository/files/{quoted_path}/raw?ref={self.ref}")
+        resp = requests.get(
+            f"{self.host}/api/v4/projects/{self.project}/respository/files/{quoted_path}/raw?ref={self.ref}"
+        )
         resp.raise_for_status()
         return resp.text()
 
     def ls(self):
-        page=1
-        progress=None
+        page = 1
+        progress = None
         while True:
-            resp = requests.get(f"{self.host}/api/v4/projects/{self.project}/repository/tree?ref={self.ref}&per_page={PER_PAGE}&page={page}&recursive=true")
+            resp = requests.get(
+                f"{self.host}/api/v4/projects/{self.project}/repository/tree?ref={self.ref}&per_page={PER_PAGE}&page={page}&recursive=true"
+            )
             resp.raise_for_status()
             page = page + 1
             arr = resp.json()
 
             if tqdm_imported:
                 if not progress:
-                    progress = tqdm.tqdm(total=int(resp.headers.get("x-total-pages")),
-                                        leave=True,
-                                        unit="files")
+                    progress = tqdm.tqdm(
+                        total=int(resp.headers.get("x-total-pages")),
+                        leave=True,
+                        unit="files",
+                    )
                 progress.update(1)
             if len(arr) == 0:
                 break
@@ -56,21 +64,22 @@ class GitlabConnector:
                 if item.get("type") == "blob":
                     yield item
         return
-    
+
     def files(self):
         """
         Get all files as a list.
         For larger repositories, could lead to memory issues.
         """
         return [f for f in self.ls()]
-        
+
 
 SNAPSHOT_GITLABS = [
     GitlabConnector(host="https://jugit.fz-juelich.de", project=7846),
-    GitlabConnector(host="https://gitlab.ebrains.eu", project=421)
+    GitlabConnector(host="https://gitlab.ebrains.eu", project=421),
 ]
 
 all_files: List[str] = []
+
 
 def validate_ebrains_dict(obj: Dict) -> List[EbrainsIdValidationResult]:
     assert isinstance(obj, dict)
@@ -83,40 +92,30 @@ def validate_ebrains_dict(obj: Dict) -> List[EbrainsIdValidationResult]:
             valid_flag = f"ebrainsquery/v3/DatasetVersion/{obj[key]}.json" in all_files
         if key == "minds/core/dataset/v1.0.0":
             valid_flag = f"ebrainsquery/v1/dataset/{obj[key]}.json" in all_files
-        if FAIL_FAST and valid_flag == False:
+        if FAIL_FAST and valid_flag is False:
             raise Exception(f"key={key},value={obj[key]}")
         if valid_flag is not None:
             return_result.append(
-                EbrainsIdValidationResult(
-                    valid=valid_flag,
-                    key=key,
-                    value=obj[key]
-                )
+                EbrainsIdValidationResult(valid=valid_flag, key=key, value=obj[key])
             )
     return return_result
+
 
 def traverse(obj: Union[Dict, List, str]) -> List[EbrainsIdValidationResult]:
 
     if isinstance(obj, dict):
         if "ebrains" in obj:
             return validate_ebrains_dict(obj["ebrains"])
-            
-        return [
-            err
-            for value in obj.values()
-            for err in traverse(value)
-        ]
+
+        return [err for value in obj.values() for err in traverse(value)]
     if isinstance(obj, list):
-        return [
-            err 
-            for item in obj
-            for err in traverse(item)
-        ]
-    
+        return [err for item in obj for err in traverse(item)]
+
     if isinstance(obj, (str, int, float, type(None))):
         return []
-    
+
     raise Exception(f"Cannot traverse {obj.__class__.__name__}")
+
 
 def main():
     global all_files
@@ -126,30 +125,35 @@ def main():
         try:
             all_files = [f.get("path") for f in gitlab.files()]
             break
-        except:
+        except Exception:
             pass
 
     print("Traversing files...")
-    all_results:List[EbrainsIdValidationResult] = []
+    all_results: List[EbrainsIdValidationResult] = []
     for dirpath, dirnames, filenames in os.walk("."):
         for filename in filenames:
             if not filename.endswith(".json"):
                 continue
             full_filename = f"{dirpath}/{filename}"
             with open(full_filename, "r") as fp:
-                validation_results = traverse(
-                    json.load(fp)
-                )
+                validation_results = traverse(json.load(fp))
 
                 for result in validation_results:
-                    result.filename=full_filename
+                    result.filename = full_filename
                 all_results.extend(validation_results)
 
-    failed=[r for r in all_results if not r.valid]
-    succeeded=[r for r in all_results if r.valid]
-    print(f"Scanned {len(all_results)}, Failed: {len(failed)}, Succeeded: {len(succeeded)}")
+    failed = [r for r in all_results if not r.valid]
+    succeeded = [r for r in all_results if r.valid]
+    print(
+        f"Scanned {len(all_results)}, Failed: {len(failed)}, Succeeded: {len(succeeded)}"
+    )
     if len(failed) > 0:
-        raise Exception('\n'.join([f'filename={f.filename}, key={f.key}, value={f.value}' for f in failed]))
+        raise Exception(
+            "\n".join(
+                [f"filename={f.filename}, key={f.key}, value={f.value}" for f in failed]
+            )
+        )
+
 
 if __name__ == "__main__":
     main()
